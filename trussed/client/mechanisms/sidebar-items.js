80eb571a3e895bfeaec75294d108da5768afdf11
nickray <nickray@users.noreply.github.com>
2021-05-02 01:24:47 +0000

    deploy: 2d3bae8dce80e3817c2fa2cd552f585a3d81b451

--- trussed/client/mechanisms/sidebar-items.js
+++ trussed/client/mechanisms/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Aes256Cbc",""],["Chacha8Poly1305",""],["Ed255",""],["HmacSha1",""],["HmacSha256",""],["HmacSha512",""],["P256",""],["Sha256",""],["Tdes",""],["Totp",""],["X255",""]]});
+initSidebarItems({"trait":[["Aes256Cbc",""],["Chacha8Poly1305",""],["Ed255",""],["HmacBlake2s",""],["HmacSha1",""],["HmacSha256",""],["HmacSha512",""],["P256",""],["Sha256",""],["Tdes",""],["Totp",""],["X255",""]]});
--- trussed/mechanisms/sidebar-items.js
+++ trussed/mechanisms/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Aes256Cbc",""],["Chacha8Poly1305",""],["Ed255",""],["HmacSha1",""],["HmacSha256",""],["HmacSha512",""],["P256",""],["P256Prehashed",""],["Sha256",""],["Tdes",""],["Totp",""],["Trng",""],["X255",""]]});
+initSidebarItems({"struct":[["Aes256Cbc",""],["Chacha8Poly1305",""],["Ed255",""],["HmacBlake2s",""],["HmacSha1",""],["HmacSha256",""],["HmacSha512",""],["P256",""],["P256Prehashed",""],["Sha256",""],["Tdes",""],["Totp",""],["Trng",""],["X255",""]]});
