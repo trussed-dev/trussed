2ed119bb8156ec6e2972383121d00229147cf7a5
Jan Nordholz <j.nordholz@tu-berlin.de>
2021-10-09 01:10:42 +0200

    (API Change) upgrade ClientId to a ClientContext struct.
    The 'service' module uses ClientContext (via pipe::ServiceEndpoint::client_ctx) to
    carry client-specific metadata. Previously, this was only the ID string, so
    a type alias for PathBuf was sufficient. With the upcoming dynamic syscall
    dispatch, the amount of client-specific state Trussed has to keep is going
    to grow significantly. This commit creates the necessary scaffolding.
    
    Minor changes:
      - in the filesystem-based software store implementations, use an explicit
        PathBuf - drop the ClientContext type import / alias redeclaration
      - pass ClientId around as a mutable reference in the reply_to() machinery,
        as we do not want to clone this structure arbitrarily when it gets large
        (as a sentinel, also stop deriving Clone for it)

--- src/service.rs
+++ src/service.rs
@@ -58,7 +58,7 @@ where
 {
     pub(crate) platform: P,
     // // Option?
-    // currently_serving: ClientId,
+    // currently_serving: ClientContext,
     // TODO: how/when to clear
     read_dir_files_state: Option<ReadDirFilesState>,
     read_dir_state: Option<ReadDirState>,
@@ -90,7 +90,11 @@ unsafe impl<P: Platform> Send for Service<P> {}
 
 impl<P: Platform> ServiceResources<P> {
     #[inline(never)]
-    pub fn reply_to(&mut self, client_id: PathBuf, request: &Request) -> Result<Reply, Error> {
+    pub fn reply_to(
+        &mut self,
+        client_id: &mut ClientContext,
+        request: &Request,
+    ) -> Result<Reply, Error> {
         // TODO: what we want to do here is map an enum to a generic type
         // Is there a nicer way to do this?
 
@@ -98,7 +102,7 @@ impl<P: Platform> ServiceResources<P> {
 
         // prepare keystore, bound to client_id, for cryptographic calls
         let mut keystore: ClientKeystore<P> = ClientKeystore::new(
-            client_id.clone(),
+            client_id.path.clone(),
             self.rng().map_err(|_| Error::EntropyMalfunction)?,
             full_store,
         );
@@ -106,7 +110,7 @@ impl<P: Platform> ServiceResources<P> {
 
         // prepare certstore, bound to client_id, for cert calls
         let mut certstore: ClientCertstore<P::S> = ClientCertstore::new(
-            client_id.clone(),
+            client_id.path.clone(),
             self.rng().map_err(|_| Error::EntropyMalfunction)?,
             full_store,
         );
@@ -114,14 +118,15 @@ impl<P: Platform> ServiceResources<P> {
 
         // prepare counterstore, bound to client_id, for counter calls
         let mut counterstore: ClientCounterstore<P::S> = ClientCounterstore::new(
-            client_id.clone(),
+            client_id.path.clone(),
             self.rng().map_err(|_| Error::EntropyMalfunction)?,
             full_store,
         );
         let counterstore = &mut counterstore;
 
         // prepare filestore, bound to client_id, for storage calls
-        let mut filestore: ClientFilestore<P::S> = ClientFilestore::new(client_id, full_store);
+        let mut filestore: ClientFilestore<P::S> =
+            ClientFilestore::new(client_id.path.clone(), full_store);
         let filestore = &mut filestore;
 
         debug_now!("TRUSSED {:?}", request);
@@ -679,8 +684,8 @@ impl<P: Platform> Service<P> {
     ) -> Result<crate::client::ClientImplementation<S>, ()> {
         use interchange::Interchange;
         let (requester, responder) = TrussedInterchange::claim().ok_or(())?;
-        let client_id = ClientId::from(client_id.as_bytes());
-        self.add_endpoint(responder, client_id)
+        let client_ctx = ClientContext::from(client_id);
+        self.add_endpoint(responder, client_ctx)
             .map_err(|_service_endpoint| ())?;
 
         Ok(crate::client::ClientImplementation::new(requester, syscall))
@@ -696,8 +701,8 @@ impl<P: Platform> Service<P> {
     ) -> Result<crate::client::ClientImplementation<&mut Service<P>>, ()> {
         use interchange::Interchange;
         let (requester, responder) = TrussedInterchange::claim().ok_or(())?;
-        let client_id = ClientId::from(client_id.as_bytes());
-        self.add_endpoint(responder, client_id)
+        let client_ctx = ClientContext::from(client_id);
+        self.add_endpoint(responder, client_ctx)
             .map_err(|_service_endpoint| ())?;
 
         Ok(crate::client::ClientImplementation::new(requester, self))
@@ -712,8 +717,8 @@ impl<P: Platform> Service<P> {
     ) -> Result<crate::client::ClientImplementation<Service<P>>, ()> {
         use interchange::Interchange;
         let (requester, responder) = TrussedInterchange::claim().ok_or(())?;
-        let client_id = ClientId::from(client_id.as_bytes());
-        self.add_endpoint(responder, client_id)
+        let client_ctx = ClientContext::from(client_id);
+        self.add_endpoint(responder, client_ctx)
             .map_err(|_service_endpoint| ())?;
 
         Ok(crate::client::ClientImplementation::new(requester, self))
@@ -722,14 +727,14 @@ impl<P: Platform> Service<P> {
     pub fn add_endpoint(
         &mut self,
         interchange: Responder<TrussedInterchange>,
-        client_id: ClientId,
+        client_ctx: ClientContext,
     ) -> Result<(), ServiceEndpoint> {
-        if client_id == PathBuf::from("trussed") {
+        if client_ctx.path == PathBuf::from("trussed") {
             panic!("trussed is a reserved client ID");
         }
         self.eps.push(ServiceEndpoint {
             interchange,
-            client_id,
+            client_ctx,
         })
     }
 
@@ -772,7 +777,7 @@ impl<P: Platform> Service<P> {
                 // #[cfg(test)] println!("service got request: {:?}", &request);
 
                 // resources.currently_serving = ep.client_id.clone();
-                let reply_result = resources.reply_to(ep.client_id.clone(), &request);
+                let reply_result = resources.reply_to(&mut ep.client_ctx, &request);
 
                 resources
                     .platform
