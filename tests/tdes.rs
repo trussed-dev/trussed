cbf8f3cc759fa79275fe06d3ce4661d6a4f306aa
Sosthène Guédon <sosthene@nitrokey.com>
2022-11-16 16:55:42 +0100

    Add key import for Tdes

--- tests/tdes.rs
+++ tests/tdes.rs
@@ -5,6 +5,7 @@ use trussed::syscall;
 
 mod client;
 
+use trussed::types::KeySerialization;
 use trussed::types::Location::*;
 use trussed::types::Mechanism;
 
@@ -14,6 +15,24 @@ use hex_literal::hex;
 fn tdes() {
     client::get(|client| {
         let key = syscall!(client.unsafe_inject_shared_key(&[48; 24], Volatile)).key;
+        let ciphertext =
+            syscall!(client.encrypt(Mechanism::Tdes, key, &[48; 8], &[], None)).ciphertext;
+
+        assert_eq!(ciphertext, hex!("f47bb46273b15eb5"),);
+
+        let plaintext = syscall!(client.decrypt(Mechanism::Tdes, key, &ciphertext, &[], &[], &[]))
+            .plaintext
+            .unwrap();
+        assert_eq!(plaintext, &[48; 8]);
+    });
+    client::get(|client| {
+        let key = syscall!(client.unsafe_inject_key(
+            Mechanism::Tdes,
+            &[48; 24],
+            Volatile,
+            KeySerialization::Raw
+        ))
+        .key;
         let ciphertext =
             syscall!(client.encrypt(Mechanism::Tdes, key, &[48; 8], &[], None)).ciphertext;
 
