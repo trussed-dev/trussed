a530ef426a5d43b2b1118e4e92af4ee74dc7872b
nickray <nickray@users.noreply.github.com>
2023-02-05 20:14:52 +0000

    deploy: 9fc19904b5970c421d81fbf28f63c13943a445b6

---
+++ trussed/backend/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["BackendId","The ID of a backend."]],"struct":[["CoreOnly","Always dispatches to the Trussed core backend."]],"trait":[["Backend","A custom backend that can override the core request implementations."],["Dispatch","Dispatches requests to custom backends."]]};
