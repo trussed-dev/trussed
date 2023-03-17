e3f26dfd5919aefe887e34c85d03b5b704f3c5bd
Sosthène Guédon <sosthene@nitrokey.com>
2023-03-17 16:53:00 +0100

    Add tests for equivalence of unwrapped keys

--- tests/chacha.rs
+++ tests/chacha.rs
@@ -1,12 +1,31 @@
 #![cfg(feature = "virt")]
 
 use trussed::client::CryptoClient;
-use trussed::types::{KeySerialization, Location, Mechanism, PathBuf, StorageAttributes};
-use trussed::{syscall, try_syscall};
+use trussed::syscall;
+use trussed::types::{
+    KeyId, KeySerialization, Location::*, Mechanism, PathBuf, SignatureSerialization,
+    StorageAttributes,
+};
 
 mod client;
 
-use trussed::types::Location::*;
+fn assert_key_eq(key1: KeyId, key2: KeyId, client: &mut impl trussed::Client) {
+    let derivative1 = syscall!(client.sign(
+        Mechanism::HmacSha256,
+        key1,
+        &[],
+        SignatureSerialization::Raw
+    ))
+    .signature;
+    let derivative2 = syscall!(client.sign(
+        Mechanism::HmacSha256,
+        key2,
+        &[],
+        SignatureSerialization::Raw
+    ))
+    .signature;
+    assert_eq!(derivative1, derivative2);
+}
 
 #[test]
 fn chacha_wrapkey() {
@@ -15,12 +34,12 @@ fn chacha_wrapkey() {
         let key = syscall!(client.unsafe_inject_key(
             Mechanism::Aes256Cbc,
             b"12345678123456781234567812345678",
-            Location::Volatile,
+            Volatile,
             KeySerialization::Raw
         ))
         .key;
 
-        let key2 = syscall!(client.generate_key(Mechanism::X255, StorageAttributes::new())).key;
+        let key2 = syscall!(client.generate_secret_key(32, Volatile)).key;
 
         let wrapped =
             syscall!(client.wrap_key(Mechanism::Chacha8Poly1305, key, key2, &[])).wrapped_key;
@@ -31,7 +50,9 @@ fn chacha_wrapkey() {
             &[],
             StorageAttributes::new()
         ))
-        .key;
+        .key
+        .unwrap();
+        assert_key_eq(key2, unwrapped, client);
 
         let wrapped_ad =
             syscall!(client.wrap_key(Mechanism::Chacha8Poly1305, key, key2, b"abc")).wrapped_key;
@@ -44,14 +65,16 @@ fn chacha_wrapkey() {
         ))
         .key
         .is_none());
-        let _unwrapped = syscall!(client.unwrap_key(
+        let unwrapped2 = syscall!(client.unwrap_key(
             Mechanism::Chacha8Poly1305,
             key,
             wrapped_ad,
             b"abc",
             StorageAttributes::new()
         ))
-        .key;
+        .key
+        .unwrap();
+        assert_key_eq(key2, unwrapped2, client);
     });
 }
 
@@ -62,21 +85,21 @@ fn chacha_wraptofile() {
         let key = syscall!(client.unsafe_inject_key(
             Mechanism::Aes256Cbc,
             b"12345678123456781234567812345678",
-            Location::Volatile,
+            Volatile,
             KeySerialization::Raw
         ))
         .key;
 
         let path = PathBuf::from("test_file");
 
-        let key2 = syscall!(client.generate_key(Mechanism::X255, StorageAttributes::new())).key;
+        let key2 = syscall!(client.generate_secret_key(32, Volatile)).key;
 
         syscall!(client.wrap_to_file(
             Mechanism::Chacha8Poly1305,
             key,
             key2,
             path.clone(),
-            Location::Volatile,
+            Volatile,
             &[],
         ));
 
@@ -84,19 +107,20 @@ fn chacha_wraptofile() {
             Mechanism::Chacha8Poly1305,
             key,
             path.clone(),
-            Location::Volatile,
-            Location::Volatile,
+            Volatile,
+            Volatile,
             &[],
         ))
         .key
         .unwrap();
+        assert_key_eq(key2, unwrapped, client);
 
         syscall!(client.wrap_to_file(
             Mechanism::Chacha8Poly1305,
             key,
             key2,
             path.clone(),
-            Location::Volatile,
+            Volatile,
             b"some ad",
         ));
 
@@ -104,8 +128,8 @@ fn chacha_wraptofile() {
             Mechanism::Chacha8Poly1305,
             key,
             path.clone(),
-            Location::Volatile,
-            Location::Volatile,
+            Volatile,
+            Volatile,
             &[],
         ))
         .key
@@ -114,12 +138,13 @@ fn chacha_wraptofile() {
         let unwrapped = syscall!(client.unwrap_from_file(
             Mechanism::Chacha8Poly1305,
             key,
-            path.clone(),
-            Location::Volatile,
-            Location::Volatile,
+            path,
+            Volatile,
+            Volatile,
             b"some ad",
         ))
         .key
         .unwrap();
+        assert_key_eq(key2, unwrapped, client);
     });
 }
