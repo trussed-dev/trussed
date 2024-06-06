a2c4070ae04eb44815c1c67ed21231845ff716e2
sosthene-nitrokey <sosthene-nitrokey@users.noreply.github.com>
2024-06-06 07:14:01 +0000

    deploy: a055e4f79a10122c8c0c882161442e6e02f0c5c6

--- zeroize/sidebar-items.js
+++ zeroize/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"derive":["Zeroize","ZeroizeOnDrop"],"struct":["Zeroizing"],"trait":["DefaultIsZeroes","TryZeroize","Zeroize","ZeroizeOnDrop"]};
+window.SIDEBAR_ITEMS = {"derive":["Zeroize","ZeroizeOnDrop"],"fn":["zeroize_flat_type"],"struct":["Zeroizing"],"trait":["DefaultIsZeroes","TryZeroize","Zeroize","ZeroizeOnDrop"]};
