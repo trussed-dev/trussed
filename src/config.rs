227add5b1e1395b314bd77dd2a5bae79362f3138
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-26 01:27:55 +0000

    Fix keystore serialization length; update config values if PQC feature enabled

--- src/config.rs
+++ src/config.rs
@@ -46,11 +46,26 @@ cfg_if::cfg_if! {
 }
 pub const MAX_SHORT_DATA_LENGTH: usize = 128;
 
-pub const MAX_SIGNATURE_LENGTH: usize = 512 * 2;
-// FIXME: Value from https://stackoverflow.com/questions/5403808/private-key-length-bytes for Rsa2048 Private key
-pub const MAX_KEY_MATERIAL_LENGTH: usize = 1160 * 2 + 72;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "pqc")] {
+        // TODO: derive these values appropriately
+        // If we're using the PQC backend, pull the max sizes from that crate.
+        pub const MAX_SIGNATURE_LENGTH: usize = 4627; //trussed_pqc_backend::sizes::MAX_SIGNATURE_LENGTH;
+        // For the PQC algorithms, public and private key are generated at the same time and stored together as
+        // the private key. Then in the derive call, it just pulls the public key from the private key store
+        // and re-saves it as a public-only key. Therefore, the max material length is both keys together, plus
+        // the PKCS8 serialization overhead.
+        pub const MAX_KEY_MATERIAL_LENGTH: usize = 7519; //trussed_pqc_backend::sizes::MAX_PRIVATE_KEY_LENGTH + trussed_pqc_backend::sizes::MAX_PUBLIC_KEY_LENGTH;
+    } else {
+        pub const MAX_SIGNATURE_LENGTH: usize = 512 * 2;
+        // FIXME: Value from https://stackoverflow.com/questions/5403808/private-key-length-bytes for Rsa2048 Private key
+        pub const MAX_KEY_MATERIAL_LENGTH: usize = 1160 * 2 + 72;
+    }
+}
 
-// must be MAX_KEY_MATERIAL_LENGTH + 4
+// Must be MAX_KEY_MATERIAL_LENGTH + 4
+// Note that this is not the serialized key material (e.g. serialized PKCS#8), but
+// the internal Trussed serialization that adds flags and such
 pub const MAX_SERIALIZED_KEY_LENGTH: usize = MAX_KEY_MATERIAL_LENGTH + 4;
 
 pub const MAX_USER_ATTRIBUTE_LENGTH: usize = 256;
