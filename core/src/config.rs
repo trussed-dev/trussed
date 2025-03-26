ae5650877a8b29ddc0a0b3e0d65b85e7b82e713d
Kyle Kotowick <kotowick@invictonlabs.com>
2025-03-26 12:19:57 -0400

    Remove pqcrypto and cfg-if dependencies

--- core/src/config.rs
+++ core/src/config.rs
@@ -21,71 +21,42 @@ pub const MAX_SERIALIZED_KEY_LENGTH: usize = MAX_KEY_MATERIAL_LENGTH + 4;
 // the PKCS8 DER encoding overhead (31 bytes).
 
 pub const MAX_SIGNATURE_LENGTH: usize = if cfg!(feature = "mldsa87") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_BYTES
+    4627
 } else if cfg!(feature = "mldsa65") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_BYTES
+    3309
 } else if cfg!(feature = "mldsa44") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES
+    2420
 } else {
     // Default from before addition of PQC
     512 * 2
 };
 
 pub const MAX_KEY_MATERIAL_LENGTH: usize = if cfg!(feature = "mldsa87") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_PUBLICKEYBYTES
-        + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_SECRETKEYBYTES
-        + 31
+    2592    // Public key
+    + 4896  // Private key
+    + 31
 } else if cfg!(feature = "mldsa65") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_PUBLICKEYBYTES
-        + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_SECRETKEYBYTES
-        + 31
+    1952    // Public key
+    + 4032  // Private key
+    + 31
 } else if cfg!(feature = "mldsa44") {
-    pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES
-        + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES
-        + 31
+    1312    // Public key
+    + 2560  // Private key
+    + 31
 } else {
     // FIXME: Value from https://stackoverflow.com/questions/5403808/private-key-length-bytes for Rsa2048 Private key
-    1160 * 2 + 72;
+    1160 * 2 + 72
 };
 
 pub const MAX_FIDO_WRAPPED_KEY_LENGTH: usize =
     if cfg!(feature = "mldsa87") || cfg!(feature = "mldsa65") || cfg!(feature = "mldsa44") {
         MAX_SERIALIZED_KEY_LENGTH + 57
     } else {
+        // Default from before addition of PQC
         128
     };
 
-// cfg_if::cfg_if! {
-//     if #[cfg(feature = "mldsa87")] {
-//         pub const MAX_SIGNATURE_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_BYTES;
-//         pub const MAX_KEY_MATERIAL_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_PUBLICKEYBYTES
-//         + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA87_CLEAN_CRYPTO_SECRETKEYBYTES
-//         + 31;
-//         //pub const MAX_MESSAGE_LENGTH: usize = MAX_KEY_MATERIAL_LENGTH;
-//         pub const MAX_FIDO_WRAPPED_KEY_LENGTH: usize =  MAX_SERIALIZED_KEY_LENGTH + 57;
-//     } else if #[cfg(feature = "mldsa65")] {
-//         pub const MAX_SIGNATURE_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_BYTES;
-//         pub const MAX_KEY_MATERIAL_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_PUBLICKEYBYTES
-//         + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA65_CLEAN_CRYPTO_SECRETKEYBYTES
-//         + 31;
-//         //pub const MAX_MESSAGE_LENGTH: usize = MAX_KEY_MATERIAL_LENGTH;
-//         pub const MAX_FIDO_WRAPPED_KEY_LENGTH: usize =  MAX_SERIALIZED_KEY_LENGTH + 57;
-//     } else if #[cfg(feature = "mldsa44")] {
-//         pub const MAX_SIGNATURE_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_BYTES;
-//         pub const MAX_KEY_MATERIAL_LENGTH: usize = pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_PUBLICKEYBYTES
-//         + pqcrypto_mldsa::ffi::PQCLEAN_MLDSA44_CLEAN_CRYPTO_SECRETKEYBYTES
-//         + 31;
-//         //pub const MAX_MESSAGE_LENGTH: usize = MAX_KEY_MATERIAL_LENGTH;
-//         pub const MAX_FIDO_WRAPPED_KEY_LENGTH: usize =  MAX_SERIALIZED_KEY_LENGTH + 57;
-//     } else {
-//         // Default from before addition of PQC
-//         pub const MAX_SIGNATURE_LENGTH: usize = 512 * 2;
-//         // FIXME: Value from https://stackoverflow.com/questions/5403808/private-key-length-bytes for Rsa2048 Private key
-//         pub const MAX_KEY_MATERIAL_LENGTH: usize = 1160 * 2 + 72;
-//         //pub const MAX_MESSAGE_LENGTH: usize = 1024;
-//         pub const MAX_FIDO_WRAPPED_KEY_LENGTH: usize = 128;
-//     }
-// }
-
 // 30 bytes are added by CBOR serialization of a FullCredential
-pub const MAX_MESSAGE_LENGTH: usize = MAX_FIDO_WRAPPED_KEY_LENGTH + 30 + 2031 + 32 + 37; // TODO: update this to use different consts for each area where this is needed
+// TODO: This was calculated by debugging and finding each location where this variable needed to be larger for one reason or another.
+// Update this to use different consts for each area where this is needed, instead of one const used everywhere.
+pub const MAX_MESSAGE_LENGTH: usize = MAX_FIDO_WRAPPED_KEY_LENGTH + 30 + 2031 + 32 + 37;
